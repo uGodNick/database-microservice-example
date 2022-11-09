666d452cea5e2f2e483ff0dbbfd804f68ac1d5d6
Nikita Kashin <ugodnickk@outlook.com>
2022-11-09 14:46:54 +0300

    fixed database initialization

--- src/modules/channel/channel.module.ts
+++ src/modules/channel/channel.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ChannelTable } from 'src/models/channel.model';
 import { ChannelController } from './channel.controller';
 import { ChannelService } from './channel.service';
 
 @Module({
+  imports: [SequelizeModule.forFeature([ChannelTable])],
   controllers: [ChannelController],
   providers: [ChannelService]
 })
--- src/modules/news/news.module.ts
+++ src/modules/news/news.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { NewsService } from './news.service';
 import { NewsController } from './news.controller';
+import { NewsTable } from 'src/models/news.model';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
+  imports: [SequelizeModule.forFeature([NewsTable])],
   providers: [NewsService],
   controllers: [NewsController]
 })
